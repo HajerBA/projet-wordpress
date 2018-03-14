9921a065eaff9ab3d9967b2adb7c2f02cd767d1b
HajerBA <hajerbnch@gmail.com>
2018-03-14 13:21:17 +0100

    update theme

---
+++ wp-content/themes/oversized/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<!-- start column2nd -->
+    <div id="column2nd">
+	<div id="column2nd-inside">
+	<div class="post">
+		<h2 class="post-title"><?php _e('Error 404',photographythemes); ?></h2>
+		<div class="entry">
+			<p><?php _e('The page you are looking for does not exist. Please check the URL for typing errors, or'); ?> <a href="<?php bloginfo('home'); ?>" title="Go Home"><?php _e('head back home'); ?></a> <?php _e('and start over'); ?></p>
+		</div>
+	</div>
+	</div>
+    </div>
+    <!-- end column2nd -->
+</div>
+<!-- end box -->
+<?php get_footer(); ?>
